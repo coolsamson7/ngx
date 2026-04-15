964a75bd86d71209ebf324d647ba010b6897195d
Andreas Ernst <andreas.ernst7@gmail.com>
2026-04-15 13:16:15 +0200

    ttt

--- openapi-generator/src/main/java/com/ngx/generator/MustacheRenderer.java
+++ openapi-generator/src/main/java/com/ngx/generator/MustacheRenderer.java
@@ -2,41 +2,34 @@ package com.ngx.generator;
 
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheException;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.github.mustachejava.MustacheFactory;
 import java.io.StringWriter;
 import java.io.IOException;
 import java.util.Map;
 
 public class MustacheRenderer {
 
-    private final DefaultMustacheFactory mf;
+    private final MustacheFactory mf;
 
     public MustacheRenderer() {
-        this.mf = new DefaultMustacheFactory();
+        // Points the factory to the 'templates' folder in your resources
+        // Now it knows where to find both main templates and partials!
+        this.mf = new DefaultMustacheFactory("templates");
     }
 
     /**
-     * Renders a template from resources/templates/{templateName} using the provided context.
+     * Renders a template using the provided context.
      *
-     * @param templateName The file name of the template (e.g., "model.mustache")
-     * @param context      A map containing the template variables
-     * @return The rendered template as a String
+     * @param templateName The name of the template (e.g., "model.mustache")
+     * @param context      The data to fill the template
+     * @return The rendered content
      */
     public String render(String templateName, Map<String, Object> context) {
         try (StringWriter writer = new StringWriter()) {
-
-            InputStream is = getClass().getClassLoader().getResourceAsStream("templates/" + templateName);
-            if (is == null) {
-                throw new MustacheException("Template not found in classpath: templates/" + templateName);
-            }
-
-            Mustache mustache = mf.compile(new InputStreamReader(is), templateName);
+            // compile() now looks relative to the "templates" path defined above
+            Mustache mustache = mf.compile(templateName);
             mustache.execute(writer, context).flush();
             return writer.toString();
-
         } catch (IOException e) {
             throw new RuntimeException("Failed to render template: " + templateName, e);
         }
